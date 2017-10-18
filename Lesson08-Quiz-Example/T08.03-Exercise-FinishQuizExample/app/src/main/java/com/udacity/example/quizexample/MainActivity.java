4fcd60e0e703af5fa67c117dedf23f367929d718
deepaknandagopal <ndeepak1993@gmail.com>
2017-10-18 02:33:37 -0400

    Completed T08.03-Solution-FinishQuizExample

--- Lesson08-Quiz-Example/T08.03-Exercise-FinishQuizExample/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
+++ Lesson08-Quiz-Example/T08.03-Exercise-FinishQuizExample/app/src/main/java/com/udacity/example/quizexample/MainActivity.java
@@ -23,6 +23,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.udacity.example.droidtermsprovider.DroidTermsExampleContract;
 
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     private int mCurrentState;
 
     private Button mButton;
+    private TextView mWord;
+    private TextView mDefinition;
 
     // This state is when the word definition is hidden and clicking the button will therefore
     // show the definition
@@ -55,8 +58,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Get the views
-        // TODO (1) You'll probably want more than just the Button
+        // Completed (1) You'll probably want more than just the Button
         mButton = (Button) findViewById(R.id.button_next);
+        mWord = (TextView) findViewById(R.id.text_view_word);
+        mDefinition = (TextView) findViewById(R.id.text_view_definition);
 
         //Run the database operation to get the cursor off of the main thread
         new WordFetchTask().execute();
@@ -87,7 +92,16 @@ public class MainActivity extends AppCompatActivity {
         // Change button text
         mButton.setText(getString(R.string.show_definition));
 
-        // TODO (3) Go to the next word in the Cursor, show the next word and hide the definition
+        // Completed (3) Go to the next word in the Cursor, show the next word and hide the definition
+        if(!mData.moveToNext()) {
+            mData.moveToFirst();
+        }
+
+        int wordCol = mData.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
+        String word = mData.getString(wordCol);
+        mWord.setText(word);
+        mDefinition.setVisibility(View.INVISIBLE);
+
         // Note that you shouldn't try to do this if the cursor hasn't been set yet.
         // If you reach the end of the list of words, you should start at the beginning again.
         mCurrentState = STATE_HIDDEN;
@@ -96,10 +110,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void showDefinition() {
 
+        mDefinition.setVisibility(View.VISIBLE);
         // Change button text
         mButton.setText(getString(R.string.next_word));
-
-        // TODO (4) Show the definition
+        int defCol = mData.getColumnIndex(DroidTermsExampleContract.COLUMN_DEFINITION);
+        String definition = mData.getString(defCol);
+        mDefinition.setText(definition);
+        // Completed (4) Show the definition
         mCurrentState = STATE_SHOWN;
 
     }
@@ -107,7 +124,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // TODO (5) Remember to close your cursor!
+        mData.close();
+        // Completed (5) Remember to close your cursor!
     }
 
     // Use an async task to do the data fetch off of the main thread.
@@ -135,8 +153,16 @@ public class MainActivity extends AppCompatActivity {
 
             // Set the data for MainActivity
             mData = cursor;
+            mData.moveToFirst();
+            int wordCol = mData.getColumnIndex(DroidTermsExampleContract.COLUMN_WORD);
 
-            // TODO (2) Initialize anything that you need the cursor for, such as setting up
+            String word = mData.getString(wordCol);
+
+            mDefinition.setVisibility(View.INVISIBLE);
+
+            mWord.setText(word);
+
+            // Completed (2) Initialize anything that you need the cursor for, such as setting up
             // the screen with the first word and setting any other instance variables
         }
     }
