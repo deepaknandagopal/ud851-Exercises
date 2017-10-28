6614bbe76f4d9ae292b5a9aa939ef3e569eada8d
deepaknandagopal <ndeepak1993@gmail.com>
2017-10-28 18:43:22 -0400

    Completed  T10.01-Exercise-IntentServices

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -1,14 +1,34 @@
 package com.example.android.background.sync;
 
-// TODO (1) Create a class called ReminderTasks
+import android.content.Context;
 
-// TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+import com.example.android.background.utilities.PreferenceUtilities;
 
-// TODO (6) Create a public static void method called executeTask
-// TODO (7) Add a Context called context and String parameter called action to the parameter list
+// Completed (1) Create a class called ReminderTasks
+public class ReminderTasks
+{
+    public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
 
-// TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+    public static void incrementWaterCount(Context context){
+        PreferenceUtilities.incrementWaterCount(context);
+    }
 
-// TODO (3) Create a private static void method called incrementWaterCount
-// TODO (4) Add a Context called context to the argument list
-// TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+    public static void executeTask(Context context, String action){
+        if(action.equals(ACTION_INCREMENT_WATER_COUNT))
+        {
+            incrementWaterCount(context);
+        }
+
+}
+}
+
+// Completed (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+
+// Completed (6) Create a public static void method called executeTask
+// Completed (7) Add a Context called context and String parameter called action to the parameter list
+
+// Completed (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+
+// Completed (3) Create a private static void method called incrementWaterCount
+// Completed (4) Add a Context called context to the argument list
+// Completed (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
